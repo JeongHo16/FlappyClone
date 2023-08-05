d8e4f83ab4dfed2ff76cea609084f21ab79e9261
JeongHo16 <cup33c@naver.com>
2023-08-05 11:21:57 +0900

    pipe generator, destroy

---
+++ Assets/_Scripts/PipeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeGenerator : MonoBehaviour
+{
+    public GameObject pipe;
+    public float time; 
+    float elapsedTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > time)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = new Vector3(1, Random.Range(-1f, 3f), 0);
+            elapsedTime = 0f;
+            Destroy(newPipe, 5f);
+        }
+    }
+}
