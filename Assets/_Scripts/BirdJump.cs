a7f6849f14f3eebd7e6f41b9010716a93dd1aebc
JeongHo16 <cup33c@naver.com>
2023-08-17 12:41:38 +0900

    sound, build setting

--- Assets/_Scripts/BirdJump.cs
+++ Assets/_Scripts/BirdJump.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class BirdJump : MonoBehaviour
 {
     Rigidbody2D rb; 
+    AudioSource ac;
     public float jumpPower;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        ac = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -17,6 +19,7 @@ public class BirdJump : MonoBehaviour
         //GetMouseButtonDown 는 스마트폰 터치와 같음.
         if (Input.GetMouseButtonDown(0)) //0 == 왼쪽
         {
+            ac.Play();
             rb.velocity = Vector2.up * jumpPower;
         }
     }
